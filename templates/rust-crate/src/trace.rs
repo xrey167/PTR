c7a9774daccd201b1f972a17fb03ed10840f7e29
Darius Rey <104269920+xrey167@users.noreply.github.com>
2026-09-18 21:50:34 +0200

    Add tracing contract to Rust crate template

---
+++ templates/rust-crate/src/trace.rs
@@ -0,0 +1,43 @@
+use std::collections::BTreeMap;
+
+#[derive(Clone, Copy, Debug, Eq, PartialEq)]
+pub enum TraceLevel {
+    Error,
+    Warn,
+    Info,
+    Debug,
+    Trace,
+}
+
+#[derive(Clone, Debug, Eq, PartialEq)]
+pub struct TraceEvent {
+    pub level: TraceLevel,
+    pub name: String,
+    pub fields: BTreeMap<String, String>,
+}
+
+impl TraceEvent {
+    pub fn new(level: TraceLevel, name: impl Into<String>) -> Self {
+        Self {
+            level,
+            name: name.into(),
+            fields: BTreeMap::new(),
+        }
+    }
+
+    pub fn with_field(mut self, key: impl Into<String>, value: impl Into<String>) -> Self {
+        self.fields.insert(key.into(), value.into());
+        self
+    }
+}
+
+pub trait TraceSink: Send + Sync {
+    fn emit(&self, event: &TraceEvent);
+}
+
+#[derive(Clone, Copy, Debug, Default)]
+pub struct NoopTraceSink;
+
+impl TraceSink for NoopTraceSink {
+    fn emit(&self, _event: &TraceEvent) {}
+}
