d6cda38c31fac45f3fd61f6d935678c641f6325a
Darius Rey <104269920+xrey167@users.noreply.github.com>
2026-09-18 21:50:21 +0200

    Model typed expected and actual error values in template

--- templates/rust-crate/src/error.rs
+++ templates/rust-crate/src/error.rs
@@ -1,5 +1,26 @@
+use crate::BackendState;
+
 #[derive(Clone, Debug, Eq, PartialEq)]
 pub enum ServiceError {
-    BackendUnavailable { backend: String },
-    Backend(String),
+    BackendUnavailable {
+        backend: String,
+    },
+    InvalidState {
+        expected: BackendState,
+        actual: BackendState,
+    },
+    Backend {
+        backend: String,
+        message: String,
+    },
+}
+
+impl ServiceError {
+    pub fn code(&self) -> &'static str {
+        match self {
+            Self::BackendUnavailable { .. } => "backend_unavailable",
+            Self::InvalidState { .. } => "invalid_state",
+            Self::Backend { .. } => "backend",
+        }
+    }
 }
