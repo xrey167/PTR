bf97f553e5b23d3b7a511195465ea4a7faf8b8d9
Darius Rey <104269920+xrey167@users.noreply.github.com>
2026-09-18 21:50:18 +0200

    Add tracing facade to Rust crate template

--- templates/rust-crate/src/lib.rs
+++ templates/rust-crate/src/lib.rs
@@ -1,9 +1,11 @@
 mod error;
 mod ports;
 mod service;
+mod trace;
 mod types;
 
 pub use error::ServiceError;
 pub use ports::Backend;
 pub use service::Service;
+pub use trace::{NoopTraceSink, TraceEvent, TraceLevel, TraceSink};
 pub use types::{BackendState, ExecuteRequest, ExecuteResult};
